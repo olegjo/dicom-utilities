d5dadddac11aea015811e6df06d574540354509e
olegunnar <oleg.johansen@gmail.com>
2025-02-12 23:04:41 +0100

    Added "installation" instructions

---
+++ dcmfind/INSTALL.md
@@ -0,0 +1,16 @@
+# INSTALLATION INSTRUCTIONS
+
+This is a very primitive instructions that I have tested on my MacBook using zshrc shell.
+
+1. Clone this git repo
+2. Open the file '~/.zshrc' using nano of vim.
+3. Append to the end of the file the following (where you edit the contents with the location of the dcmfind.py script from the repo from step 1)
+
+```
+####################
+### My own stuff ###
+
+# dicom-utilities git repo
+alias dcmfind='python3 /Users/olegunnar/Documents/Jobb/NIL/dev/dicom-utilities/dcmfind/dcmfind.py'
+
+```
--- dcmfind/dcmfind.py
+++ dcmfind/dcmfind.py
@@ -1,38 +1,63 @@
 import glob, sys, pydicom, os, glob, fnmatch
 import progressbar, argparse
+from typing import Optional
 
 import pydicom.errors
 
 class MatchedSeries:
-    def __init__(self, ds: pydicom.Dataset, dicom_file):
+    def __init__(self, ds: pydicom.Dataset):
         self.ds = ds
-        self.dicom_file = dicom_file
         self.series_instance_uid = ds["SeriesInstanceUID"]
+        try:
+            self.patient_name = ds["PatientName"].value
+        except(KeyError):
+            self.patient_name = "UNKNOWN"
+
+        self.file_paths = []
     
     def __eq__(self, other: pydicom.Dataset):
-        return other["SeriesInstanceUID"] == self.series_instance_uid
-    
-    def __repr__(self):
         try:
-            patient_name = self.ds['PatientName'].value
+            patient_name = other["PatientName"].value
         except(KeyError):
-            patient_name = "UNKNOWN PATIENT NAME"
-        
+            patient_name = "UNKNOWN"
+
+        return other["SeriesInstanceUID"] == self.series_instance_uid and patient_name == self.patient_name
+    
+    def __repr__(self):        
         try:
             series_description = self.ds['SeriesDescription'].value
         except(KeyError):
             series_description = "UNKNOWN SERIES DESCRIPTION"
 
-        return f"{patient_name} -- {series_description} -- {self.dicom_file}"
+        ret = "="*100
+        ret += f"\nPatientName         {self.patient_name}"
+        ret += f"\nSeriesDescription   {series_description}"
+        ret += f"\nFound in the following locations:"
 
-def check_match(matches, check):
+        unique_dirnames = []
+        for f in self.file_paths:
+            dirname = os.path.abspath(os.path.dirname(f))
+            if dirname not in unique_dirnames:
+                unique_dirnames.append(dirname)
+        
+        for dirname in unique_dirnames:
+            ret += f"\n\t\t- {dirname}"
+        return ret
+    
+    def add_file(self, path):
+        self.file_paths.append(path)
+
+def check_match(matches, check) -> Optional[MatchedSeries]:
     for m in matches:
         if m == check:
-            return True
-    return False
+            return m
+    return None
 
-def dcmfindseries(input_dir, search_tag, search_value, case_insensitive, verbose=False, show_progress_bar=True):
+def dcmfindseries(input_dir, search_tag, search_value, case_insensitive, show_progress_bar=True):
     matches: list[MatchedSeries] = []
+
+    if case_insensitive:
+        search_value = str(search_value).lower()
 
     # All files and directories in the given directory
     all_files = glob.glob(f"{input_dir}/**/*", recursive=True)
@@ -44,7 +69,8 @@ def dcmfindseries(input_dir, search_tag, search_value, case_insensitive, verbose
         # Setup progressbar
         pbar = progressbar.ProgressBar(max_value=len(all_files))
         pbar_i = 0
-
+    
+    # Loop through all dicom files and check if query is fulfilled
     for dicom_file in all_files:
         if show_progress_bar:
             pbar.update(pbar_i+1)
@@ -56,23 +82,24 @@ def dcmfindseries(input_dir, search_tag, search_value, case_insensitive, verbose
             continue
 
         try:
-            looked_up_value = ds[search_tag].value
-        except(KeyError):
+            looked_up_value = str(ds[search_tag].value)
+        except(KeyError) as e:
             continue
 
         if case_insensitive:
             looked_up_value = str(looked_up_value).lower()
-            search_tag = str(search_value).lower()
 
         # Look for the search value in the looked up value. Can have wildcards.
         if fnmatch.fnmatch(looked_up_value, search_value):
             ## Check if this series is already found
             already_found = check_match(matches, ds)
 
-            if not already_found:
-                matches.append(MatchedSeries(ds, dicom_file))
-                if verbose:
-                    print(f"\n\nFOUND: {dicom_file}\n\n")
+            if already_found is None:
+                m = MatchedSeries(ds)
+                m.add_file(dicom_file)
+                matches.append(m)
+            else:
+                m.add_file(dicom_file)
 
     return matches
 
@@ -83,18 +110,32 @@ dcmfind -q "SeriesDescription==*ABC*" directory
 """
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser_usagetext = """
+This tool can be used to find DICOM files or directories containing DICOM files that fulfil a given search query.
+For example, if you have a folder structure that contains DICOM files, and you are looking for all DICOM files 
+where the SeriesDescription equals "My_series_description", you can search like this:
+
+\t>> dcmfind -q "SeriesDescription==My_series_description" /path/to/folder
+
+The dcmfind tool will recursively find all files in /path/to/folder that fulfils the search criterion.
+
+You may also use wildcards:
+
+\t>> dcmfind -q "SeriesDescription==*_series_" /path/to/folder
+
+This will return all folders that contain DICOM files, who's Series Descriptions match the wildcard query *_series_*
+
+"""
+    parser = argparse.ArgumentParser(prog="dcmfind", epilog=parser_usagetext, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("directory")
     parser.add_argument("-q", "--query")
     parser.add_argument("--case-insensitive", default=False, action='store_true')
-    parser.add_argument("-v", "--verbose", default=False, action='store_true')
     args = parser.parse_args()
 
     [search_term, search_value] = args.query.split("==")
-    verbose = args.verbose
     case_insensitive = args.case_insensitive
 
-    matches = dcmfindseries(args.directory, search_term, search_value, case_insensitive, verbose, True)
+    matches = dcmfindseries(args.directory, search_term, search_value, case_insensitive, True)
 
     print()
     print()
